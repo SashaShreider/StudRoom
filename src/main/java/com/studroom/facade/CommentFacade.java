7c2713b85c35c4355411ddab910a93be0f60da06
WaveWalker <sasha20032205@mail.ru>
2024-05-05 23:26:06 +0300

    Добавление сервисов

---
+++ src/main/java/com/studroom/facade/CommentFacade.java
@@ -0,0 +1,19 @@
+package com.studroom.facade;
+
+import com.studroom.dto.CommentDTO;
+import com.studroom.entity.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentFacade {
+
+    public CommentDTO commentToCommentDTO(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setUsername(comment.getUsername());
+        commentDTO.setMessage(comment.getMessage());
+
+        return commentDTO;
+    }
+
+}
---
+++ src/main/java/com/studroom/web/AuthController.java
@@ -0,0 +1,65 @@
+package com.studroom.web;
+
+import com.studroom.payload.reponse.JWTTokenSuccessResponse;
+import com.studroom.payload.reponse.MessageResponse;
+import com.studroom.payload.request.LoginRequest;
+import com.studroom.payload.request.SignupRequest;
+import com.studroom.security.JWTTokenProvider;
+import com.studroom.security.SecurityConstants;
+import com.studroom.services.UserService;
+import com.studroom.validations.ResponseErrorValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/auth")
+@PreAuthorize("permitAll()")
+public class AuthController {
+
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private ResponseErrorValidation responseErrorValidation;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<Object> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword()
+        ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTTokenSuccessResponse(true, jwt));
+    }
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@Valid @RequestBody SignupRequest signupRequest, BindingResult bindingResult) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+
+        userService.createUser(signupRequest);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+}
